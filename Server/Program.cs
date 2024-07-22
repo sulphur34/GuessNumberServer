47a061481b3314ffd94ad449f490ca45a38585e2
sulphur34 <71759429+sulphur34@users.noreply.github.com>
2024-07-22 18:35:47 +0300

    Add server project

---
+++ Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using Grains.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Server
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string waitingQueueKey = "GlobalQueue";
+            uint requiredPlayersCount = 2;
+
+            using var host = Host.CreateDefaultBuilder(args)
+                .UseOrleans(siloBuilder =>
+                {
+                    siloBuilder.UseLocalhostClustering();
+                    siloBuilder.AddMemoryGrainStorage("GameStorage")
+                        .AddMemoryGrainStorageAsDefault();
+                })
+                .Build();
+
+            await host.StartAsync();
+
+            var client = host.Services.GetRequiredService<IGrainFactory>();
+
+            var waitingQueue = client.GetGrain<IWaitingQueueGrain>(waitingQueueKey);
+
+            await waitingQueue.Configure(requiredPlayersCount);
+
+            Console.WriteLine("Setup completed.");
+            Console.WriteLine("Now you can launch the client.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            await host.StopAsync();
+        }
+    }
+}
+
